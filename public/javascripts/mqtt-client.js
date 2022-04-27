caa60872a8fb9e21f58d869b89a45685b0dfacb4
Matias Vereecke <matias.vereecke@gmail.com>
2022-04-27 13:28:35 +0200

    adding dashboard

--- public/javascripts/mqtt-client.js
+++ public/javascripts/mqtt-client.js
@@ -10,7 +10,9 @@ const hostname = '127.0.0.1'
 const port = 3001
 
 const server = http.createServer((req, res) => {
-  res.render('index');
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hello World')
 
 })
 
